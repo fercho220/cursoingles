b5f341f864f8dd0d75b696baee511fbe4cc29fc6
fercho220 <141796391+fercho220@users.noreply.github.com>
2025-07-10 16:03:53 -0600

    Update wsgi.py

--- CursoIngles/wsgi.py
+++ CursoIngles/wsgi.py
@@ -1,16 +1,13 @@
-"""
-WSGI config for CursoIngles project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
-"""
+# CursoIngles/wsgi.py
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CursoIngles.settings')
 
+# Se obtiene la aplicación de Django
 application = get_wsgi_application()
+
+# Se "envuelve" la aplicación con WhiteNoise
+application = WhiteNoise(application)
